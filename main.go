235e32d620bf720f98975d178c2915af49a83f27
Ngina <ngina@users.noreply.github.com>
2024-07-31 16:48:52 -0700

    Update snippetView handler to accept an id query parameter

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id := r.URL.Query().Get("id")
+	if i, err := strconv.Atoi(id); err != nil || i < 0 {
+		log.Printf("unacceptable query parameter: [id=%#v]", id)
+		http.NotFound(w, r)
+		return
+	} else {
+		fmt.Fprintf(w, "Display a specific snippet with ID %d...", i)
+		return
+	}
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +32,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	//update our application so that the /snippet/create route only responds to
 	// HTTP requests which use the POST method, like so:
 	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
@@ -33,7 +44,7 @@ func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view/", snippetView)
+	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
