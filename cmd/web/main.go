ce4e4a791115e4270edbb906481248b7b8524296
Ngina <ngina@users.noreply.github.com>
2024-07-31 22:35:53 -0700

    Refactor to create handler file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,43 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	if i, err := strconv.Atoi(id); err != nil || i < 0 {
-		log.Printf("unacceptable query parameter: [id=%#v]", id)
-		http.NotFound(w, r)
-		return
-	} else {
-		fmt.Fprintf(w, "Display a specific snippet with ID %d...", i)
-		return
-	}
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-
-	//update our application so that the /snippet/create route only responds to
-	// HTTP requests which use the POST method, like so:
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
-}
 
 func main() {
 
