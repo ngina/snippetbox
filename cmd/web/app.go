bc80df784716992f03ef9bf31a9e6317946f0f94
Ngina <ngina@users.noreply.github.com>
2024-08-04 14:28:32 -0700

    Refactor to use app routes()

---
+++ cmd/web/app.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger 
+}
+
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,51 +7,23 @@ import (
 	"os"
 )
 
-type application struct {
-	errorLog *log.Logger
-	infoLog *log.Logger 
-}
-
 func main() {
-	// Define a new command-line flag with the name 'addr', a default value of ":4000" 
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this *before* you use the addr variable
-	// otherwise it will always contain the default value of ":4000". If any errors are // encountered during parsing the application will be terminated.
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)		
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	app := &application{
-		infoLog: infoLog,
+		infoLog:  infoLog,
 		errorLog: errorLog,
-
 	}
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory. // Note that the path given to the http.Dir function is relative to the project // directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
-
-	srv := &http.Server{ 
-		Addr: *addr,
+	srv := &http.Server{
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: mux, 
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on : %s", *addr)
