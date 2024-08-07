213366414c127a35bcf2d535d728eda461e9db9d
Ngina <ngina@users.noreply.github.com>
2024-08-06 21:42:26 -0700

    Log each incoming request

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,10 +8,10 @@ func (app *application) routes() http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
