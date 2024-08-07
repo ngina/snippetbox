ad8e48a043e64f1d781c116a6d2089719d02f6eb
Ngina <ngina@users.noreply.github.com>
2024-08-06 17:48:36 -0700

    Auto parse pages in partial folder

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -36,22 +36,29 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// and assign it to the name variable.
 		name := filepath.Base(page)
 
-		// Create a slice containing the filepaths for our base template, any // partials and the page.
-		files := []string{
-			"./ui/html/base.tmpl.html",
-			"./ui/html/partials/nav.tmpl.html",
-			page,
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
 		}
-		// Parse the files into a template set.
-		ts, err := template.ParseFiles(files...)
+
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
 			return nil, err
 
 		}
-		// Add the template set to the map, using the name of the page // (like 'home.tmpl') as the key.
+
+		// Call ParseFiles() *on this template set* to add the page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the map, using the name of the page
+		// (like 'home.tmpl') as the key.
 		cache[name] = ts
 	}
 
-	// Return the map.
 	return cache, nil
 }
